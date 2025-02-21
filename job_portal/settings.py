8e78b64857b2c4f55603ab8f609c3332c5ccece5
Muhammad Iktear <iktear500@gmail.com>
2025-02-21 17:24:00 +0600

    first commit

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -42,11 +42,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://job-portal-backend-opal.vercel.app/",
+    "https://job-portal-backend-opal.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://job-portal-backend-opal.vercel.app/'
+    'https://job-portal-backend-opal.vercel.app'
 ]
 
 
